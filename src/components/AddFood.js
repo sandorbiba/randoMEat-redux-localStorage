b3d4f5ea370f22a47807753920eaba7f708c9773
Sandor Biba <sandorbiba@gmail.com>
2020-05-01 12:51:26 +0200

    add AddFood

--- src/components/AddFood.js
+++ src/components/AddFood.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddFood = () => {
-  return <div>AddFood</div>;
+  const [foodName, setFoodName] = useState("");
+  const [foodDetails, setFoodDetails] = useState("");
+
+  const SaveFood = (e) => {
+    e.preventDefault();
+    console.log(foodName, foodDetails);
+  };
+
+  return (
+    <form onSubmit={SaveFood}>
+      <label>
+        Food name:
+        <input
+          value={foodName}
+          onChange={(event) => setFoodName(event.target.value)}
+          name="foodname"
+          type="text"
+        />
+      </label>
+      <br />
+      <label>
+        List of ingredients / recipe:
+        <input
+          value={foodDetails}
+          onChange={(event) => setFoodDetails(event.target.value)}
+          name="fooddetails"
+          type="longtext"
+        />
+      </label>
+      <br />
+      <button>Submit</button>
+    </form>
+  );
 };
