3ed8e86d3464ab1fc746aa1dddf203a0d6986431
Sandor Biba <sandorbiba@gmail.com>
2020-05-10 11:08:10 +0200

    add Typical

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
+import Typical from "react-typical";
 
 export const Home = () => {
   return (
-    <div>
+    <div className="center">
+      <Typical
+        steps={[
+          "Hello 👋",
+          1000,
+          "I am react-typical",
+          1000,
+          "I can support emoji 👌",
+          1000,
+          "I can only delete what needs deleting",
+          1000,
+          "I can pause",
+          4000,
+          "I can loop",
+          1000,
+          "I can wait",
+          3000,
+          "I can vary typing speed",
+          2000,
+        ]}
+        loop={Infinity}
+        wrapper="h2"
+        className={"padding"}
+      />
       <div>
         <Link to="/random">randoMEat</Link>
       </div>
